f115694800115b863e81f5ab5359758f1b791643
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-05-26 16:38:27 +0300

    Add string extractor for getting strings from file

---
+++ FullTextIndex/Utils/StringExtractor.cs
@@ -0,0 +1,14 @@
+﻿namespace FullTextIndex;
+
+public class StringExtractor
+{
+    public static IEnumerable<string> ArticleSet() => ReadArticleSet("articles1.txt");
+
+    private static IEnumerable<string> ReadArticleSet(string fileName)
+    {
+        using var reader = new StreamReader(Path.Combine(@"D:\dotnet-lessons\full-text-index\assets", fileName));
+
+        while (reader.ReadLine() is { } line)
+            yield return line;
+    }
+}
