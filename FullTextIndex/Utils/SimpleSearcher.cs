2c63d65e5290ceef9df4e773ce680db61f24a573
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-05-26 16:38:07 +0300

    Update simple searcher to work with full text index and benchmarks

---
+++ FullTextIndex/Utils/SimpleSearcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FullTextIndex;
+
+public class SimpleSearcher
+{
+    public IEnumerable<string> Search(string word, string item)
+    {
+        var pos = 0;
+        while (true)
+        {
+            pos = item.IndexOf(word, pos, StringComparison.InvariantCulture);
+
+            if (pos >= 0)
+                yield return FormatMatchText(item, pos);
+            else 
+                break;
+                
+            pos++;
+        }
+    }
+    
+    public IEnumerable<string> Search(string word, IEnumerable<string> stringsToSearch)
+    {
+        foreach (var item in stringsToSearch)
+        {
+            foreach (var match in Search(word, item))
+                yield return match;
+        }
+    }
+
+    private string FormatMatchText(string text, int pos)
+    {
+        var start = Math.Max(0, pos - 50);
+        var end = Math.Min(start + 100, text.Length - 1);
+        
+        return string.Concat(
+            start == 0 ? "" : "...",
+            text.AsSpan(start, end - start), 
+            end == text.Length - 1 ? "" : "...");
+    }
+}
