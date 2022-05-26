93a2e04601073981f35e6f8e7aa141c6b7e7e5e3
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-05-26 16:32:29 +0300

    Add simple full text index

---
+++ FullTextIndex/Indexes/SimpleFullTextIndex.cs
@@ -0,0 +1,38 @@
+namespace FullTextIndex;
+
+public class SimpleFullTextIndex
+{
+    private readonly Dictionary<string, HashSet<int>> _index = new();
+    private readonly List<string> _content = new();
+    private readonly Lexer _lexer = new();
+    private readonly SimpleSearcher _searcher = new();
+
+    public void AddStringToIndex(string text)
+    {
+        var documentId = _content.Count;
+        
+        foreach (var token in _lexer.GetTokens(text))
+        {
+            if (_index.TryGetValue(token, out var set))
+                set.Add(documentId);
+            else
+                _index.Add(token, new HashSet<int>() {documentId});
+        }
+        
+        _content.Add(text);
+    }
+
+    public IEnumerable<int> Search(string word) => 
+        _index.TryGetValue(word.ToLowerInvariant(), out var set) ? set : Enumerable.Empty<int>();
+
+    public IEnumerable<string> SearchTest(string word)
+    {
+        var documentIds = Search(word);
+
+        foreach (var documentId in documentIds)
+        {
+            foreach (var match in _searcher.Search(word, _content[documentId]))
+                yield return match;
+        }
+    }
+}
