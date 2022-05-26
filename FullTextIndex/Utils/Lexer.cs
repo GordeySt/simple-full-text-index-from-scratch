8727a0bbb10c9846cb0310296a2372319614c120
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-05-26 16:32:43 +0300

    Add lexer for full text index

---
+++ FullTextIndex/Utils/Lexer.cs
@@ -0,0 +1,29 @@
+namespace FullTextIndex;
+
+public class Lexer
+{
+    public IEnumerable<string> GetTokens(string text)
+    {
+        var start = -1;
+
+        for (var i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetterOrDigit(text[i]))
+            {
+                if (start == -1)
+                    start = i;
+            }
+            else
+            {
+                if (start >= 0)
+                {
+                    yield return GetToken(text, i, start);
+                    start = -1;
+                }
+            }
+        }
+    }
+
+    private string GetToken(string text, int i, int start) => 
+        text.Substring(start, i - start).Normalize().ToLowerInvariant();
+}
