d01ddd891ff9a7751be43e6c969a249590ef8042
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-06-02 19:13:22 +0300

    Refactor code

---
+++ PrefixSuffixRadix/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using PrefixSuffixRadix;
+using PrefixSuffixRadix.Benchmarks;
+using PreparationToStream.FullText.Database;
+
+var ctx = new DatabaseContext();
+var arr = ctx.Words.Select(x => x.Word1).ToArray();
+var prefix = new PrefixTree(arr!);
+var result = prefix.StartsWith("like").ToArray();
+
+foreach (var item in result)
+    Console.WriteLine(item);
+
+// BenchmarkRunner.Run<StartsWithBenchmark>();
